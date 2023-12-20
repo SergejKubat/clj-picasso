f18b1a11d9bfbeb67e1636f6afe0e0f34c719e20
Sergej Kubat <sergej.kubat18@gmail.com>
2023-12-20 01:22:05 +0100

    Image loading and saving

--- src/clj_picasso/core.clj
+++ src/clj_picasso/core.clj
@@ -1,1 +1,24 @@
-(ns clj-picasso.core)
+(ns clj-picasso.core
+  (:import (java.awt.image BufferedImage)
+           [javax.imageio ImageIO]
+           [java.io File IOException]))
+
+(defn load-image [file-path]
+  "This function loads an image from a file."
+  (try
+    (let [image (ImageIO/read (File. (str file-path)))]
+      image)
+    (catch IOException ex
+           (println (str "Error loading image: " (.getMessage ex)))
+      nil)))
+
+(defn save-image [image file-path]
+  "Save the given image to the specified file path."
+  (try
+    (ImageIO/write ^BufferedImage image "png" (File. (str file-path)))
+    (catch IOException ex
+      (println "Error saving image:" (.getMessage ex)))))
+
+(def image (load-image "./resources/images/input.png"))
+
+(save-image image "./resources/images/output.png")
